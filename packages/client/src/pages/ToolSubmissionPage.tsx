3b0182f3aa3338698b26783ee1c33fa0b9321460
elliotchun <eschun@uci.edu>
2025-12-01 00:15:52 -0800

    Add tool creator field and update form spacing

--- packages/client/src/pages/ToolSubmissionPage.tsx
+++ packages/client/src/pages/ToolSubmissionPage.tsx
@@ -1,12 +1,12 @@
-import { Button, Field, Heading, Input, Link, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { Button, Field, Heading, HStack, Input, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
 import { Form } from "react-router-dom";
 
 function ToolSubmissionPage() {
-  const FormInput = ({ label, required = true }: { label: ReactNode, required?: boolean }) => (
+  const FormInput = ({ label, required = true }: { label: string, required?: boolean }) => (
     <Field.Root required={required}>
       <Field.Label>
-        {label} <Field.RequiredIndicator />
+        <Text>{label}</Text>
+        <Field.RequiredIndicator />
       </Field.Label>
       <Input />
     </Field.Root>
@@ -18,32 +18,44 @@ function ToolSubmissionPage() {
         Tool Submission Form
       </Heading>
       <Form>
-        <FormInput label="Email" />
-        <FormInput label="Link to Tool" />
-        <FormInput label="Description" />
-        <FormInput label="Compatability Information" required={false} />
-        <FormInput label="Tutorial Video(s)" required={false} />
-        <FormInput label="Creator's Guidelines/Cautions for the Tool" required={false} />
-        <FormInput label="Limitations" required={false} />
-        <FormInput label="Extra Comments" required={false} />
-        <RadioGroup.Root>
-          <VStack align="start">
-            <RadioGroup.Item value="Yes">
-              <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
-              <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
-              <RadioGroup.ItemText>Yes</RadioGroup.ItemText>
-            </RadioGroup.Item>
-            <RadioGroup.Item value="No">
-              <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
-              <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
-              <RadioGroup.ItemText>No</RadioGroup.ItemText>
-            </RadioGroup.Item>
-          </VStack>
-        </RadioGroup.Root>
-        <Button variant="outline">Submit</Button>
+        <VStack gap={4} align="stretch">
+          <FormInput label="Email" />
+          <FormInput label="Link to Tool" />
+          <FormInput label="Description" />
+          <FormInput label="Compatability Information" required={false} />
+          <FormInput label="Tutorial Video(s)" required={false} />
+          <FormInput label="Creator's Guidelines/Cautions for the Tool" required={false} />
+          <FormInput label="Limitations" required={false} />
+          <FormInput label="Extra Comments" required={false} />
+          <Field.Root required>
+            <Field.Label>
+              <Text>Are you the creator of this tool?</Text>
+              <Field.RequiredIndicator />
+            </Field.Label>
+            <RadioGroup.Root>
+              <VStack align="start">
+                <HStack align="center" justifyContent="space-between">
+                  <RadioGroup.Item value="Yes" w="1/2">
+                    <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
+                    <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
+                    <RadioGroup.ItemText>Yes, provide email: </RadioGroup.ItemText>
+                  </RadioGroup.Item>
+                  <Input type="email" placeholder="Email" w="1/2" />
+                </HStack>
+                <RadioGroup.Item value="No">
+                  <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
+                  <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
+                  <RadioGroup.ItemText>No</RadioGroup.ItemText>
+                </RadioGroup.Item>
+              </VStack>
+            </RadioGroup.Root>
+          </Field.Root>
+          <Button variant="outline">Submit</Button>
+        </VStack>
       </Form>
-    </Stack>
+    </Stack >
   );
 }
 
 export default ToolSubmissionPage;
+
