8a269e78dbeeb6024b4d7965e8afd48b21c3674b
elliotchun <eschun@uci.edu>
2025-12-20 15:54:26 -0800

    Add backend endpoint to handle form submissions. Add schema for form data.

--- packages/client/src/pages/ToolSubmissionPage.tsx
+++ packages/client/src/pages/ToolSubmissionPage.tsx
@@ -1,14 +1,24 @@
 import { Box, Button, Field, Heading, HStack, Input, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
-import { Form } from "react-router-dom";
 
 function ToolSubmissionPage() {
-  const TextInput = ({ label, required = true }: { label: string, required?: boolean }) => (
+  const submitForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const res = await fetch("http://localhost:5173/api/tool", {
+      method: "POST",
+      body: formData,
+    });
+  };
+
+  const TextInput = ({ name, label, required = true }: { name: string, label: string, required?: boolean }) => (
     <Field.Root required={required}>
       <Field.Label>
         <Text>{label}</Text>
         <Field.RequiredIndicator />
       </Field.Label>
-      <Input />
+      <Input name={name} />
     </Field.Root>
   );
 
@@ -17,34 +27,34 @@ function ToolSubmissionPage() {
       <Heading size="4xl">
         Tool Submission Form
       </Heading>
-      <VStack>
-        <Box w="sm" alignSelf="start">
-          <Form>
+      <form onSubmit={submitForm}>
+        <VStack>
+          <Box w="sm" alignSelf="start">
             <VStack gap={4} align="stretch">
-              <TextInput label="Email" />
-              <TextInput label="Link to Tool" />
-              <TextInput label="Description" />
-              <TextInput label="Compatability Information" required={false} />
-              <TextInput label="Tutorial Video(s)" required={false} />
-              <TextInput label="Creator's Guidelines/Cautions for the Tool" required={false} />
-              <TextInput label="Limitations" required={false} />
-              <TextInput label="Extra Comments" required={false} />
+              <TextInput name="email" label="Email" />
+              <TextInput name="link" label="Link to Tool" />
+              <TextInput name="description" label="Description" />
+              <TextInput name="compatability" label="Compatability Information" required={false} />
+              <TextInput name="videos" label="Tutorial Video(s)" required={false} />
+              <TextInput name="guidelines" label="Creator's Guidelines/Cautions for the Tool" required={false} />
+              <TextInput name="limits" label="Limitations" required={false} />
+              <TextInput name="comments" label="Extra Comments" required={false} />
               <Field.Root required>
                 <Field.Label>
                   <Text>Are you the creator of this tool?</Text>
                   <Field.RequiredIndicator />
                 </Field.Label>
-                <RadioGroup.Root>
+                <RadioGroup.Root name="isCreator">
                   <VStack align="start">
                     <HStack align="center" justifyContent="space-between">
-                      <RadioGroup.Item value="Yes" w="1/2">
+                      <RadioGroup.Item value="true" w="1/2">
                         <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
                         <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
                         <RadioGroup.ItemText>Yes, provide email: </RadioGroup.ItemText>
                       </RadioGroup.Item>
                       <Input type="email" placeholder="Email" w="1/2" />
                     </HStack>
-                    <RadioGroup.Item value="No">
+                    <RadioGroup.Item value="false">
                       <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
                       <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
                       <RadioGroup.ItemText>No</RadioGroup.ItemText>
@@ -53,10 +63,10 @@ function ToolSubmissionPage() {
                 </RadioGroup.Root>
               </Field.Root>
             </VStack>
-          </Form>
-        </Box>
-        <Button variant="outline" borderColor="primary" borderWidth="thin" w="sm" alignSelf="end">Submit</Button>
-      </VStack>
+          </Box>
+          <Button type="submit" variant="outline" borderColor="primary" borderWidth="medium" size="2xl" w="2xs" alignSelf="end">Submit</Button>
+        </VStack>
+      </form>
     </Stack >
   );
 }
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,40 @@
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./auth";
+import { body } from "express-validator";
+import multer from "multer";
+import { ToolFormModel, type ToolFormParameters } from "./schema/tool";
 
 const app = express();
 const port = 8000;
+const formHandler = multer();
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/api/test", (req, res) => {
-    res.send("Hello test")
-});
+    res.send("Hello!")
+})
+app.post("/api/tool",
+    formHandler.none(),
+    body("email").trim().isEmail().normalizeEmail(),
+    body("link").trim().isURL().escape(),
+    body("description").isString().trim().escape(),
+    body("compatability").optional().trim().escape(),
+    body("videos").optional().trim().escape(),
+    body("guidelines").optional().trim().escape(),
+    body("limits").optional().trim().escape(),
+    body("comments").optional().trim().escape(),
+    body("isCreator").isBoolean(),
+    async (req, res) => {
+        try {
+            await ToolFormModel.create(req.body as ToolFormParameters);
+            return res.status(201).send("Success");
+        } catch (err) {
+            return res.status(500);
+        }
+    });
 
 app.listen(port, () => {
     console.log(`Better Auth app listening on port ${port}`);
