2ef11dcb33a54ffcfaf8f21f26cb82f59e3b4424
elliotchun <eschun@uci.edu>
2026-01-31 16:18:53 -0800

    Update .env: Must provide valid URI to database server

--- packages/server/auth.ts
+++ packages/server/auth.ts
@@ -3,10 +3,13 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import 'dotenv/config'
 
-if (!process.env.MONGODB_USERNAME) throw new Error("MONGODB_USERNAME not provided");
-if (!process.env.MONGODB_PASSWORD) throw new Error("MONGODB_PASSWORD not provided");
-const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.upfmf6g.mongodb.net/?appName=Cluster0`;
-console.log(`Logging into mongo with ${MONGODB_URI}`)
+if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI not provided");
+    process.exit(1);
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log(`Logging into mongo with ${MONGODB_URI}`);
 
 const client = new MongoClient(MONGODB_URI, {
     serverApi: ServerApiVersion.v1,
