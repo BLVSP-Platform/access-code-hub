71c9a8fec28b55c72cf167e62107ea786cd3271b
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-02-11 01:10:56 -0800

    re-add default MONGO_URI

--- packages/server/auth.ts
+++ packages/server/auth.ts
@@ -1,11 +1,10 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { client } from "./db"
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://cluster0.upfmf6g.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&appName=Cluster0'
 
-// if (!MONGODB_URI) throw new Error("");
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/blvsp'
 
-// const credentials = "C:\\Users\\green\\Downloads\\X509-cert-9033981711947858141.pem";
+if (!MONGODB_URI) throw new Error("");
 
 export const auth = betterAuth({
     database: mongodbAdapter(client.db(), {
