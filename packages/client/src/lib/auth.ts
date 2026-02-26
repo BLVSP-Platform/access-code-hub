4b5062c5ff2cecd9dddda4b3480423855940e512
elliotchun <eschun@uci.edu>
2026-02-26 01:52:44 -0800

    Load env file using vite

--- packages/client/src/lib/auth.ts
+++ packages/client/src/lib/auth.ts
@@ -1,5 +1,4 @@
 import { createAuthClient } from "better-auth/react"
-import "dotenv/config";
 
 export const { signIn, signUp, useSession } = createAuthClient({
     baseURL: import.meta.env.CLIENT_URL
