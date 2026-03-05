5ad803fb5ad96af5cea297adf2b4f7630652d84b
elliotchun <eschun@uci.edu>
2026-03-04 18:49:52 -0800

    Handle PostThreadPage form submission using react-hook-form

--- packages/client/src/pages/PostThreadPage.tsx
+++ packages/client/src/pages/PostThreadPage.tsx
@@ -1,80 +1,114 @@
-import { Portal, Dialog, Input, Button, Flex, Stack, Textarea, Heading, Box, Field } from "@chakra-ui/react"
+import { FormDialog } from "@/components/FormDialog";
+import { Input, Button, Flex, Stack, Textarea, Heading, Box, Field } from "@chakra-ui/react"
+import { useState } from "react";
+import { FieldError, RegisterOptions, useForm } from "react-hook-form"
+
+interface ThreadData {
+    title: string;
+    topic: string;
+    content: string;
+    tags: string;
+}
+
+type TName = "title" | "topic" | "content" | "tags";
 
 interface PostPageProps {
     label: string;
     text: string;
+    name: TName;
+    registerOptions: RegisterOptions<ThreadData, TName> | undefined;
+    error: FieldError | undefined;
+
 }
 
-const submitDialog = (
-          <Dialog.Root>
-            <Dialog.Trigger asChild>
-                <Flex gap="4" w="100%" justify="flex-end" mt="8">
-                <Button 
-                borderColor="primary" 
-                _hover={{ bg: "primary", color: "white" }}
-                size="xl" 
-                variant="outline" 
-                type="submit">
-                    Submit
-                </Button>
-        </Flex>
-            </Dialog.Trigger>
-                <Portal>
-                    <Dialog.Backdrop>
-                        <Dialog.Positioner>
-                            <Dialog.Content>
-                                <Dialog.Header><Dialog.Title>Thread Posted!</Dialog.Title></Dialog.Header>
-                                <Dialog.Footer>
-                                    <Dialog.ActionTrigger asChild>
-                                        <Button 
-                                        variant="outline"
-                                        borderColor="primary" 
-                                        _hover={{ bg: "primary", color: "white" }}>
-                                            Close
-                                        </Button>
-                                    </Dialog.ActionTrigger>
-                                </Dialog.Footer>
-                            </Dialog.Content>
-                        </Dialog.Positioner>
-                    </Dialog.Backdrop>
-                </Portal>
-            </Dialog.Root>
-    )
-
-const PostPageItem = ({ label, text }: PostPageProps) => {
-    return (
-        <Field.Root orientation="horizontal" required>
-            <Field.Label>
-                {label} <Field.RequiredIndicator />
-            </Field.Label>
-            <Input placeholder={text} />
-        </Field.Root>
-    );
-};
-
 function PostThreadPage() {
-    return (
-        <>
-        <Stack align="flex-start" gap={8} w="80%">
-            <Heading as="h1" size="4xl">
-                Post a Thread
-            </Heading>
+    const [dialogOpen, setDialogOpen] = useState(false);
 
-            <PostPageItem label="Title:" text="Title your post" />
-            <PostPageItem label="Topic:" text="Ex. Programming IDEs" />
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ThreadData>();
 
-            <Field.Root orientation="horizontal" required>
+    // @TODO: hook up to backend
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+        setDialogOpen(true);
+    });
+
+    const PostPageInput = ({ label, text, name, registerOptions, error }: PostPageProps) => {
+        return (
+            <Field.Root orientation="horizontal" required invalid={!!error}>
                 <Field.Label>
-                    Content: <Field.RequiredIndicator />
+                    {label} <Field.RequiredIndicator />
                 </Field.Label>
-                <Textarea resize="none" />
+                <Input placeholder={text} {...register(name, registerOptions)} />
+                <Field.ErrorText>{error?.message}</Field.ErrorText>
             </Field.Root>
+        );
+    };
 
-            <PostPageItem label="Tag(s):" text="Ex. Python, Java, Programming" />
-        </Stack>
+    return (
+        <form onSubmit={onSubmit}>
+            <Stack align="flex-start" gap={8} w="80%">
+                <Heading as="h1" size="4xl">
+                    Post a Thread
+                </Heading>
 
-        {submitDialog}
-        </>
+                <PostPageInput
+                    label="Title:"
+                    text="Title your post"
+                    name="title"
+                    registerOptions={{ required: "A title is required" }}
+                    error={errors.title}
+                />
+                <PostPageInput
+                    label="Topic:"
+                    text="Ex. Programming IDEs"
+                    name="topic"
+                    registerOptions={{ required: "A topic is required" }}
+                    error={errors.topic}
+                />
+
+                <Field.Root orientation="horizontal" required>
+                    <Field.Label>
+                        Content: <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Textarea
+                        resize="none"
+                        {...register("content", { required: "Content is required" })}
+                    />
+                </Field.Root>
+
+                <PostPageInput
+                    label="Tag(s):"
+                    text="Ex. Python, Java, Programming"
+                    name="tags"
+                    registerOptions={{ required: "At least one tag is required" }}
+                    error={errors.tags}
+                />
+            </Stack>
+            <FormDialog
+                title="Thread Posted!"
+                body=""
+                isOpen={dialogOpen}
+                onOpenChange={details => {
+                    if (!details.open) setDialogOpen(false)
+                }}
+            >
+                <Flex gap="4" w="100%" justify="flex-end" mt="8">
+                    <Button
+                        borderColor="primary"
+                        _hover={{ bg: "primary", color: "white" }}
+                        size="xl"
+                        variant="outline"
+                        type="submit">
+                        Submit
+                    </Button>
+                </Flex>
+            </FormDialog>
+        </form>
+
     )
 }
 
--- packages/client/src/pages/VolunteerPage.tsx
+++ packages/client/src/pages/VolunteerPage.tsx
@@ -1,6 +1,7 @@
-import { Portal, Dialog, Flex, Button, Field, Input, Stack, Textarea, Heading} from "@chakra-ui/react"
-import {useForm} from "react-hook-form"
-import {useState} from "react"
+import { Flex, Button, Field, Input, Stack, Textarea, Heading } from "@chakra-ui/react"
+import { useForm } from "react-hook-form"
+import { useState } from "react"
+import { FormDialog } from "@/components/FormDialog";
 
 interface VolunteerData {
     shortAnswer: string;
@@ -9,48 +10,21 @@ interface VolunteerData {
 
 function VolunteerPage() {
     const [dialogOpen, setDialogOpen] = useState(false);
-    
+
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm<VolunteerData>();
-    
+
     // @TODO: hook up to backend
     const onSubmit = handleSubmit((data) => {
         console.log(data);
         setDialogOpen(true);
-    })
-
-    const submitDialog = (
-          <Dialog.Root placement="center"open={dialogOpen} onOpenChange={(e) => setDialogOpen(e.open)}>
-                <Portal>
-                    <Dialog.Backdrop>
-                        <Dialog.Positioner>
-                            <Dialog.Content>
-                                <Dialog.Header><Dialog.Title>Thank you for volunteering!</Dialog.Title></Dialog.Header>
-                                <Dialog.Body>
-                                    A moderator will review this and get back to you.
-                                </Dialog.Body>
-                                <Dialog.Footer>
-                                    <Dialog.ActionTrigger asChild>
-                                        <Button 
-                                        variant="outline"
-                                        borderColor="primary" 
-                                        _hover={{ bg: "primary", color: "white" }}>
-                                            Close
-                                        </Button>
-                                    </Dialog.ActionTrigger>
-                                </Dialog.Footer>
-                            </Dialog.Content>
-                        </Dialog.Positioner>
-                    </Dialog.Backdrop>
-                </Portal>
-            </Dialog.Root>
-    )
+    });
 
     return (
-        <form onSubmit={onSubmit} noValidate>
+        <form onSubmit={onSubmit}>
             <Stack gap="8" align="flex-start" maxW="70%">
                 <Heading as="h1" size="4xl">
                     Volunteering
@@ -64,7 +38,7 @@ function VolunteerPage() {
                     <Textarea
                         resize="none"
                         size="xl"
-                        {...register("shortAnswer", {required: "An answer is required"})}
+                        {...register("shortAnswer", { required: "An answer is required" })}
                     />
                     <Field.ErrorText>{errors.shortAnswer?.message}</Field.ErrorText>
                 </Field.Root>
@@ -75,24 +49,30 @@ function VolunteerPage() {
                         <Field.RequiredIndicator />
                     </Field.Label>
                     <Input
-                        {...register("email", {required: "Email is required"})}
+                        {...register("email", { required: "Email is required" })}
                     />
                     <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
                 </Field.Root>
             </Stack>
-
-            <Flex gap="4" w="100%" justify="flex-end" mt="8">
-                <Button 
-                    borderColor="primary" 
-                    _hover={{ bg: "primary", color: "white" }}
-                    size="xl" 
-                    variant="outline" 
-                    type="submit">
+            <FormDialog
+                title="Thank you for volunteering!"
+                body="A moderator will review this and get back to you."
+                isOpen={dialogOpen}
+                onOpenChange={details => {
+                    if (!details.open) setDialogOpen(false);
+                }}
+            >
+                <Flex gap="4" w="100%" justify="flex-end" mt="8">
+                    <Button
+                        borderColor="primary"
+                        _hover={{ bg: "primary", color: "white" }}
+                        size="xl"
+                        variant="outline"
+                        type="submit">
                         Submit
-                </Button>
-            </Flex>
-
-            {submitDialog}
+                    </Button>
+                </Flex>
+            </FormDialog>
         </form>
     )
 }
