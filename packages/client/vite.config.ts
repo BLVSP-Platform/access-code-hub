e1ba29b7c7f6be98941edc4a7175b9c860b445e9
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-03-12 01:00:23 -0700

    biome

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_");
-  return {
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_SERVER_URL,
-          changeOrigin: true
-        }
-      }
-    }
-  }
-})
+	const env = loadEnv(mode, process.cwd(), "VITE_");
+	return {
+		define: {
+			__APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_SERVER_URL,
+					changeOrigin: true,
+				},
+			},
+		},
+	};
+});
--- packages/server/auth.ts
+++ packages/server/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
-import { MongoClient, ServerApiVersion } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import "dotenv/config";
 
 if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI not provided");
@@ -8,18 +8,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 console.log(`Logging into mongo with ${MONGODB_URI}`);
 
 const client = new MongoClient(MONGODB_URI, {
-    serverApi: ServerApiVersion.v1,
+	serverApi: ServerApiVersion.v1,
 });
 
 const db = client.db();
 
-if (!process.env.CLIENT_URL) throw new Error("CLIENT_URL not provided")
+if (!process.env.CLIENT_URL) throw new Error("CLIENT_URL not provided");
 export const auth = betterAuth({
-    database: mongodbAdapter(db, {
-        client
-    }),
-    emailAndPassword: {
-        enabled: true,
-    },
-    trustedOrigins: [process.env.CLIENT_URL]
+	database: mongodbAdapter(db, {
+		client,
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	trustedOrigins: [process.env.CLIENT_URL],
 });
