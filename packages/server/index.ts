e05195cf5025f64c92b6c10633e33a34bd1c3704
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-05-07 02:02:22 -0700

    fix: set trustedOrigins and use splat for express5

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,17 +13,15 @@ const port = Number(process.env.PORT);
 
 const corsOptions = {
 	origin: ["http://localhost:5173", "https://ach-frontend-production.up.railway.app"],
-	credentials: true,
+	redentials: true,
 };
 
 app.use(cors(corsOptions));
 
-app.options("*", cors(corsOptions));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
-
-app.all("/api/auth/*", toNodeHandler(auth));
 app.use("/api", apiRouter);
 
 app.listen(port, "0.0.0.0", () => {
