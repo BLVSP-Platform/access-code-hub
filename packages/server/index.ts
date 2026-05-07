c276312df72fc3391637304af1c3120dfe154818
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-05-07 01:54:29 -0700

    fix: cors preflight

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,17 +11,19 @@ await initializeDatabase();
 const app = express();
 const port = Number(process.env.PORT);
 
-app.use(
-	cors({
-		origin: ["http://localhost:5173", "https://ach-frontend-production.up.railway.app"],
-		credentials: true,
-	}),
-);
+const corsOptions = {
+	origin: ["http://localhost:5173", "https://ach-frontend-production.up.railway.app"],
+	credentials: true,
+};
 
-app.all("/api/auth/*", toNodeHandler(auth));
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+app.all("/api/auth/*", toNodeHandler(auth));
 app.use("/api", apiRouter);
 
 app.listen(port, "0.0.0.0", () => {
