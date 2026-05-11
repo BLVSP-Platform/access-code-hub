1c254ad46cbecd8df751426d67b3d0d27e0d5ec0
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-05-11 00:28:02 -0700

    fix: add allowedhosts to preview

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig(({ mode }) => {
 				"@": path.resolve(__dirname, "./src"),
 			},
 		},
+		preview: {
+			allowedHosts: [env.VITE_CLIENT_URL],
+		},
 		server: {
 			proxy: {
 				"/api": {
