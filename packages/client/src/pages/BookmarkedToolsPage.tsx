7f40c31ba1eb6345368ac006d98dae379091ee3b
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-05-19 19:10:16 -0700

    feat: bookmarked tools page

--- packages/client/src/pages/BookmarkedToolsPage.tsx
+++ packages/client/src/pages/BookmarkedToolsPage.tsx
@@ -1,11 +1,92 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Box, Heading, Link, RatingGroup, Stack, Table, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import type { Tool } from "./ToolIndexPage";
 
 function BookmarkedToolsPage() {
+	const [bookmarkedTools, setBookmarkedTools] = useState<Tool[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchTools = async () => {
+			try {
+				const res = await fetch("/api/tools/bookmarks/me");
+				const data = await res.json();
+				const tools = data.map((b: any) => b.toolId);
+				setBookmarkedTools(tools);
+
+				setBookmarkedTools(tools);
+			} catch (err) {
+				console.error("Failed to fetch tools:", err);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchTools();
+	}, []);
+
+	if (loading) {
+		return <Text>Loading....</Text>;
+	}
+
 	return (
 		<Stack>
 			<Heading as="h1" size="4xl">
 				My Bookmarked Tools
 			</Heading>
+
+			<Box overflowX="auto" width="100%">
+				<Table.Root
+					size="lg"
+					variant="outline"
+					showColumnBorder
+					css={{
+						"--chakra-colors-border": "#5B5B5B",
+						_dark: {
+							"--chakra-colors-border": "#5e5e5e",
+						},
+					}}
+				>
+					<Table.Header>
+						<Table.Row bg="secondary">
+							<Table.ColumnHeader>Name</Table.ColumnHeader>
+							<Table.ColumnHeader>Compatibility</Table.ColumnHeader>
+							<Table.ColumnHeader>Description</Table.ColumnHeader>
+							<Table.ColumnHeader>Overall Ratings</Table.ColumnHeader>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+						{bookmarkedTools.map((tool) => (
+							<Table.Row key={tool._id} bg="tertiary" width="100%">
+								<Table.Cell>
+									<Link href={`/tools/${tool.slug}`}>{tool.name}</Link>
+								</Table.Cell>
+								<Table.Cell>{tool.compatibility}</Table.Cell>
+								<Table.Cell maxWidth="500px">
+									<Text>{tool.description}</Text>
+								</Table.Cell>
+								<Table.Cell>
+									<RatingGroup.Root
+										readOnly
+										count={5}
+										value={tool.rating}
+										size="sm"
+										css={{
+											"--chakra-colors-bg-emphasized": "#b1b1b1",
+											_dark: {
+												"--chakra-colors-bg-emphasized": "#605d70",
+											},
+										}}
+									>
+										<RatingGroup.HiddenInput />
+										<RatingGroup.Control />
+									</RatingGroup.Root>
+								</Table.Cell>
+							</Table.Row>
+						))}
+					</Table.Body>
+				</Table.Root>
+			</Box>
 		</Stack>
 	);
 }
