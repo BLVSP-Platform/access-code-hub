dea3c6d9acc0cceb0fc737ef11ee4e1ffd13037a
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-06-03 13:52:43 -0700

    chore: disable pino logging

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,5 @@
 import { toNodeHandler } from "better-auth/node";
 import express from "express";
-import pinoHttp from "pino-http";
 import { auth } from "./auth";
 import apiRouter from "./routes/api";
 import "dotenv/config";
@@ -14,15 +13,15 @@ await initializeDatabase();
 const app = express();
 const port = Number(process.env.PORT);
 
-const logger = pinoHttp({
-	transport: {
-		target: "pino-pretty",
-		options: { colorize: true },
-	},
-	redact: ["headers.cookie"],
-});
+// const logger = pinoHttp({
+// 	transport: {
+// 		target: "pino-pretty",
+// 		options: { colorize: true },
+// 	},
+// 	redact: ["headers.cookie"],
+// });
 
-app.use(logger);
+// app.use(logger);
 
 const corsOptions = {
 	origin: [process.env.CLIENT_URL],
