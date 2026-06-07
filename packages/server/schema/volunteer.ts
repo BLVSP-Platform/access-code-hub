823a622b65ec6e4212bfa7879d4f6754cb1b25e2
Jeremiah Soe <soejeremiah@gmail.com>
2026-06-07 01:00:15 -0700

    Admin View: See Volunteer Submissions (#83)
    * feat: modify volunteer form schema
    
    * feat: admin router and routes
    
    * feat: admin page and volunteer tab
    
    * fix: correct route
    
    * fix: permissions
    
    * fix: lookup user instead of using userid

--- packages/server/schema/volunteer.ts
+++ packages/server/schema/volunteer.ts
@@ -5,13 +5,19 @@ export const volunteerFormSchema = new mongoose.Schema({
 	userId: { type: String, required: true },
 	email: { type: String, required: true },
 	shortAnswer: { type: String, required: true },
+	status: {
+		type: String,
+		enum: ["pending", "approved", "rejected"],
+		default: "pending",
+	},
+	createdAt: { type: Date, default: Date.now },
 });
 
 export const VolunteerFormModel = mongoose.model("volunteer", volunteerFormSchema);
 
 export type VolunteerFormParameters = InferSchemaType<typeof volunteerFormSchema>;
 
-export const insertVolunteerApplication = async (formParams: VolunteerFormParameters) => {
+export const insertVolunteerApplication = async (formParams: Omit<VolunteerFormParameters, "status" | "createdAt">) => {
 	const result = await VolunteerFormModel.create(formParams);
 	return result._id;
 };
